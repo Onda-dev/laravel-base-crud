7d0fa7b8cbbe8165ed044bd9deef9dd6b81f7070
Onda-dev <manuelaltieri3@gmail.com>
2022-07-14 17:31:05 +0200

    Seed prova

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Comics;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -11,6 +12,22 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
+
+        foreach($comics as $comic) {
+            $newComic = new Comic();
+        }
+
+        $newComic = new Comic();
+
+        $newComic->title = $comic['title'];
+        $newComic->description = $comic['description'];
+        $newComic->price = $comic['price'];
+        $newComic->series = $comic['series'];
+        $newComic->sale_date = $comic['sale_date'];
+        $newComic->comic_book = $comic['comic_book'];
+        $newComic->image = $comic['thumb'];
+
+        $newComic->save();
     }
 }
